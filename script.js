f2609da5d1fd7134e693b2f7ce81a3d24d793546
Seth rearick <srearick@selectmedical.com>
2024-07-25 16:32:58 -0400

    Add variable to calculate dimension of squares/pixels.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let canvas = document.querySelector(".etchasketch");
 
 let numSquares = 5;
+let squareDimension = 300 / numSquares;
 
 for (row=0; row<numSquares; row++) {
     for (square=0; square<numSquares; square++) {
         let pixel = document.createElement("div");
         pixel.className = ".pixel";
-        pixel.style.width = `${300 / numSquares}px`;
-        pixel.style.height = `${300 / numSquares}px`;
+        pixel.style.width = `${squareDimension}px`;
+        pixel.style.height = `${squareDimension}px`;
 
         canvas.appendChild(pixel);
     }
