c370161146f680ceab75e72150fda3cf0d8bc977
Cannonball <rearick.seth@gmail.com>
2024-07-30 19:21:13 -0400

    Add draw and erase event handlers.
    Added a mouseover event handler that changes the class to
    .activated-pixel. Added a contextmenu (rightclick) event handler that
    changes the class to .pixel.

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 let canvas = document.querySelector(".etchasketch");
 
-let numSquares = 5;
+let numSquares = 100;
 let squareDimension = 300 / numSquares;
 
 for (row=0; row<numSquares; row++) {
     for (square=0; square<numSquares; square++) {
         let pixel = document.createElement("div");
-        pixel.className = ".pixel";
+        pixel.className = "pixel";
         pixel.style.width = `${squareDimension}px`;
         pixel.style.height = `${squareDimension}px`;
+
+        pixel.addEventListener("mouseover", 
+            (event) => {
+                event.target.className = "activated-pixel";
+            }
+        );
+
+        pixel.addEventListener("contextmenu", 
+            (event) => {
+                event.preventDefault();
+                event.target.className = "pixel";
+            }
+        );
 
         canvas.appendChild(pixel);
     }
